37e431e6a05ab0ab885aea638ad57d341e21cd7e
matthewds02 <descheppermatthew@gmail.com>
2022-12-31 17:04:08 +0100

    Headsets component better

--- src/components/Headsets.js
+++ src/components/Headsets.js
@@ -3,16 +3,10 @@ import '../styles/products.css';
 import ProductCard from '../components/ProductsCard';
 import {Section} from "./Section";
 
-function Headset(props) {
-    const {headset} = props;
-
-    return <ProductCard product={headset}/>;
-}
-
 export function Headsets(props) {
     const {headsets, title} = props;
 
     return <Section title={title}>
-        {headsets.map(product => <Headset key={product.name} headset={product}/>)}
+        {headsets.map(headset => <ProductCard key={headset.name} product={headset}/>)}
     </Section>;
 }
