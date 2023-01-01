b0b2cef8843532c5643392ddea9dadd879fef926
matthewds02 <descheppermatthew@gmail.com>
2023-01-01 21:58:57 +0100

    Homepage cleaner and extra component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,33 +1,40 @@
 import React from 'react';
 import '../styles/products.css';
 import {PICTURES_AIRPODS, PICTURES_GALAXYBUDS, PICTURES_HUAWEIBUDS, PICTURES_ONEPLUSBUDS} from "../data/data";
-import {ApplePage} from "./ApplePage";
 import {Col, Container, Row} from "react-bootstrap";
-import {SamsungPage} from "./SamsungPage";
-import {HuaweiPage} from "./HuaweiPage";
-import {OnePlusPage} from "./OnePlusPage";
+import {Headsets} from "../components/Headsets";
 
+// Deze functie retourneert een rij met een Headsets-component
+function HeadsetRow(props) {
+    return (
+        <Row>
+            <Headsets headsets={props.headsets} title={props.title}/>
+        </Row>
+    );
+}
+
+// Maak een lijst met de gegevens voor elke rij
+const rows = [
+    {headsets: PICTURES_AIRPODS, title: "Apple Airpods"},
+    {headsets: PICTURES_GALAXYBUDS, title: "Galaxy Buds"},
+    {headsets: PICTURES_HUAWEIBUDS, title: "Huawei FreeBuds"},
+    {headsets: PICTURES_ONEPLUSBUDS, title: "OnePlus Buds"}
+];
+
+// De HomePage-functie retourneert de hoofdpagina van de applicatie
 export function HomePage() {
-
     return (
         <Container>
+            {/* Render een rij met een koptekst */}
             <Row className="my-4">
                 <Col>
-                    <h1>all wireless headphones</h1>
+                    <h1>alle draadloze hoofdtelefoons</h1>
                 </Col>
             </Row>
-            <Row>
-                <ApplePage airpods={PICTURES_AIRPODS} />
-            </Row>
-            <Row>
-                <SamsungPage galaxyBuds={PICTURES_GALAXYBUDS} />
-            </Row>
-            <Row>
-                <HuaweiPage huaweiBuds={PICTURES_HUAWEIBUDS} />
-            </Row>
-            <Row>
-                <OnePlusPage onePlusBuds={PICTURES_ONEPLUSBUDS} />
-            </Row>
+            {/* Render elke rij in de lijst met behulp van de HeadsetRow-functie */}
+            {rows.map((row) => (
+                <HeadsetRow key={row.headsets} headsets={row.headsets} title={row.title}/>
+            ))}
         </Container>
     );
 }
